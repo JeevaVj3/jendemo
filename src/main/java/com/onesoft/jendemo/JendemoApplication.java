4fd1ba228db8972bd5663838ddbbaf64e7b1e26d
Jeeva <famousjeeva@gmail.com>
2023-11-21 16:58:45 +0530

    done

---
+++ src/main/java/com/onesoft/jendemo/JendemoApplication.java
@@ -0,0 +1,13 @@
+package com.onesoft.jendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JendemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JendemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onesoft/jendemo/JendemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onesoft.jendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JendemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
